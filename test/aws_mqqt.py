807b7fc652538d7e1023875f85e4ceccdfd5c8da
Sam Stein <sam@mega.net>
2019-04-18 11:24:44 -0700

    Update aws_mqqt.py

--- test/aws_mqqt.py
+++ test/aws_mqqt.py
@@ -1,4 +1,14 @@
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
 
 IOT_ENDPOINT = 'a2z6jgzt0eip8f-ats.iot.us-west-2.amazonaws.com'
 
