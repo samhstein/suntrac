4b5c84808dc708880a096df2c08b35e24cab4f50
Sam Stein <sam@mega.net>
2019-04-18 11:29:19 -0700

    Update aws_mqqt.py

--- test/aws_mqqt.py
+++ test/aws_mqqt.py
@@ -28,10 +28,13 @@ myMQTTClient.configureCredentials(
     "/home/pi/suntrac/certs/devices/90c9b2ae5d-private.pem.key",
     "/home/pi/suntrac/certs/devices/90c9b2ae5d-certificate.pem.crt"
     )
-myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
 myMQTTClient.connect()
 myMQTTClient.publish("iot/helo", {'proc_id': proc_id}, 0)
