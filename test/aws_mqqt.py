286174b7befe5b467a411b2825a729c3c4dc202d
Sam Stein <sam@mega.net>
2019-04-18 11:06:58 -0700

    Create aws_mqqt.py

---
+++ test/aws_mqqt.py
@@ -0,0 +1,30 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+IOT_ENDPOINT = 'a2z6jgzt0eip8f-ats.iot.us-west-2.amazonaws.com'
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+myMQTTClient = AWSIoTMQTTClient('beta')
+myMQTTClient.configureEndpoint(IOT_ENDPOINT, 8883)
+myMQTTClient.configureCredentials(
+    "/home/pi/suntrac/certs/root/AmazonRootCA1.pem",
+    "/home/pi/suntrac/certs/devices/90c9b2ae5d-private.pem.key",
+    "/home/pi/suntrac/certs/devices/90c9b2ae5d-certificate.pem.crt"
+    )
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+myMQTTClient.connect()
+myMQTTClient.publish("iot/helo", {'proc_id': proc_id}, 0)
+myMQTTClient.subscribe("iot/helo", 1, customCallback)
+myMQTTClient.unsubscribe("iot/helo")
+myMQTTClient.disconnect()
