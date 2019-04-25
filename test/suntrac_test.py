8ee529e9eeaea9961ff819287cc7ec979d4053d0
Sam Stein <sam@mega.net>
2019-04-25 13:41:47 -0700

    Update suntrac_test.py

--- test/suntrac_test.py
+++ test/suntrac_test.py
@@ -22,111 +22,117 @@ def get_temp_c(volts):
     return steinhart
 
 def waitPush():
-  while(GPIO.input(27) == 1):
-    time.sleep(0.1)
-  while(GPIO.input(27) == 0):
-    time.sleep(0.1)
+    while(GPIO.input(27) == 1):
+        time.sleep(0.1)
+    while(GPIO.input(27) == 0):
+        time.sleep(0.1)
 
 def waitRelease():
-  while(GPIO.input(27) == 0):
-    time.sleep(0.1)
+    while(GPIO.input(27) == 0):
+        time.sleep(0.1)
 
 def checkPush():
-  if GPIO.input(27) == 0 :
-    return 1
-  return 0
+    if GPIO.input(27) == 0 :
+        return 1
+    return 0
 
 def acc():
-  ACC_ADDRESS = 0x1d
-  MAG_ADDRESS = 0x1e
-  WHO_AM_I = 0x0f
-  bus = smbus.SMBus(1)
-  ret = 0
+    ACC_ADDRESS = 0x1d
+    MAG_ADDRESS = 0x1e
+    WHO_AM_I = 0x0f
+    bus = smbus.SMBus(1)
+    ret = 0
 
-  try:
-    me = bus.read_byte_data( ACC_ADDRESS, WHO_AM_I)
-    if me != 0x41:
-      print ("Fail to read Accelerometer! read: " + str(me))
-      ret+= 1
-  except Exception as e:
-    print("Fail to read Accelerometer! read: " + str(e))
-    ret+=1
-  try:
-    me = bus.read_byte_data( MAG_ADDRESS, WHO_AM_I)
-    if me != 0x3d:
-      print ("Fail to read Magnetometer! read: " + str(me))
-      ret+=1
-  except Exception as e:
-      print ("Fail to read Magnetometer! read: " + str(e))
-  if ret == 0 :
-    print("LSM303 tested OK")
-  else:
-    print("LSM303 test FAIL!")
-  return ret
+    try:
+        me = bus.read_byte_data( ACC_ADDRESS, WHO_AM_I)
+        if me != 0x41:
+            print ("Fail to read Accelerometer! read: " + str(me))
+            ret+= 1
+    except Exception as e:
+        print("Fail to read Accelerometer! read: " + str(e))
+        ret+=1
+    try:
+        me = bus.read_byte_data( MAG_ADDRESS, WHO_AM_I)
+        if me != 0x3d:
+            print ("Fail to read Magnetometer! read: " + str(me))
+            ret+=1
+    except Exception as e:
+        print ("Fail to read Magnetometer! read: " + str(e))
 
-def led():
-  IO_EXP_ADD = 0x20
-  IO_EXP_DIR = 0x03
-  IO_EXP_OUT = 0x01
-  ledList = [0, 0x12, 0x24, 0x48, 0x7e, 0x10, 0x20, 0x40, 0x02, 0x04, 0x08, 0x00]
-  bus = smbus.SMBus(1)
-  ret = 0
-  waitRelease()
-  try:
-    bus.write_byte_data(IO_EXP_ADD, IO_EXP_DIR, 0x00) #set output
-  except Exception as e:
-    print("Fail to communicate with I/O expander! " + str(e))
-    ret+=1
+    if ret == 0 :
+        print("LSM303 tested OK")
+    else:
+        print("LSM303 test FAIL!")
+
     return ret
 
-  print("Check all color on both LED then press the onboard push-button")
-  loop = True
-  while loop:
-    for i in ledList:
-      time.sleep(0.5)
-      bus.write_byte_data(IO_EXP_ADD, IO_EXP_OUT, ~i) #set output
-      if GPIO.input(27) == 0:
-        loop = False
-        break
+def led():
+    IO_EXP_ADD = 0x20
+    IO_EXP_DIR = 0x03
+    IO_EXP_OUT = 0x01
+    ledList = [0, 0x12, 0x24, 0x48, 0x7e, 0x10, 0x20, 0x40, 0x02, 0x04, 0x08, 0x00]
+    bus = smbus.SMBus(1)
+    ret = 0
+    waitRelease()
+    try:
+        bus.write_byte_data(IO_EXP_ADD, IO_EXP_DIR, 0x00) #set output
+    except Exception as e:
+        print("Fail to communicate with I/O expander! " + str(e))
+        ret+=1
+        return ret
 
-  bus.write_byte_data(IO_EXP_ADD, IO_EXP_OUT, 0xff)
-  return ret
+    print("Check all color on both LED then press the onboard push-button")
+    loop = True
+    while loop:
+        for i in ledList:
+            time.sleep(0.5)
+            bus.write_byte_data(IO_EXP_ADD, IO_EXP_OUT, ~i) #set output
+            if GPIO.input(27) == 0:
+                loop = False
+                break
+
+            bus.write_byte_data(IO_EXP_ADD, IO_EXP_OUT, 0xff)
+
+    return ret
 
 def gsm():
-  ret = 0;
-  try:
-    ser = serial.Serial(
-               port='/dev/ttyS0',
-               baudrate = 115200,
-               parity=serial.PARITY_NONE,
-               stopbits=serial.STOPBITS_ONE,
-               bytesize=serial.EIGHTBITS,
-               timeout=.5
-           )
+    ret = 0;
+    try:
+        ser = serial.Serial(
+            port='/dev/ttyS0',
+            baudrate = 115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=.5
+        )
     #ser = serial.Serial('/dev/ttyAMA0', 115200)
-  except Exception as e:
-    print("Fail to open serial port! " + str(e))
-    return 1
-  cmd = "AT\r\n"
-  try:
-    ser.flushInput()
-    ser.flushOutput()
-    ser.write(cmd.encode('ascii'))
-    lineCount = 0
-    gret = ser.readline().decode('ascii')
-    lineCount = 1
-    gret = ser.readline().decode('ascii')
-  except Exception as e:
-    if(lineCount == 0):
-      print("Failed to read response" + str(e))
-      ret = 1
-  if(gret.find('OK') >= 0):
-    print("GSM/GPS module SIM868 test success")
-  else:
-    print("GSM/GPS module SIM868 test fail! " + gret)
-    ret = 1
-  ser.close()
-  return ret
+    except Exception as e:
+        print("Fail to open serial port! " + str(e))
+        return 1
+
+    cmd = "AT\r\n"
+    try:
+        ser.flushInput()
+        ser.flushOutput()
+        ser.write(cmd.encode('ascii'))
+        lineCount = 0
+        gret = ser.readline().decode('ascii')
+        lineCount = 1
+        gret = ser.readline().decode('ascii')
+    except Exception as e:
+        if(lineCount == 0):
+            print("Failed to read response" + str(e))
+            ret = 1
+
+    if(gret.find('OK') >= 0):
+        print("GSM/GPS module SIM868 test success")
+    else:
+        print("GSM/GPS module SIM868 test fail! " + gret)
+        ret = 1
+
+    ser.close()
+    return ret
 
 def term():
     v1 = m.get_adc_volt(1)
@@ -146,10 +152,10 @@ def motor():
     set_motors(0) 	# turn off all motors
     sleep(.2)
     print('turning on motor 1')
-	set_motors(1)	# turn motor 1 on and motor 2 off
+    set_motors(1)	# turn motor 1 on and motor 2 off
     sleep(2)
     print('turning on motor 2')
-	set_motors(2)	# turn motor 1 on and motor 2 off
+    set_motors(2)	# turn motor 1 on and motor 2 off
     sleep(2)
     print('turning off both motors...')
     set_motors(0) 	# turn off all motors
