3db6d6c97918a8d8a78aac6c1600c2d3717dca46
Sam Stein <sam@mega.net>
2019-05-22 15:23:12 -0700

    Update suntrac.py

--- panel/suntrac.py
+++ panel/suntrac.py
@@ -6,5 +6,6 @@ client = Client(('localhost', 11211),
     serializer=serde.python_memcache_serializer,
     deserializer=serde.python_memcache_deserializer)
 
+print(client.get('suntrac_reading'))
 result = str(client.get('suntrac_reading'))
 sys.stdout.write(result.replace("'", '"'))
