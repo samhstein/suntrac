0ccc9e405bb2034ad1ed976eb2192f6acc083245
Sam Stein <sam@mega.net>
2019-06-20 13:31:56 -0700

    gps

---
+++ gprs/gps.py
@@ -0,0 +1,27 @@
+import serial
+
+ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+print(ser)
+
+print('sending: ', 'ATZ')
+ser.write(b'ATZ\r\n')
+
+print('reading')
+s = ser.readline()
+
+
+#print('got: ', s)
+
+#ser.write(b'AT+CGATT=1')
+
+#ser.write(b'AT+SAPBR=3,1,"CONTYPE","GPRS"')
+
+#ser.write(b'AT+SAPBR=3,1,"APN","jtm2m"')
+
+#ser.write(b'AT+SAPBR=1,1')
+
+#ser.write(b'AT+SAPBR=2,1')
+
+#ser.write(b'AT+CIPGSMLOC=1,1')
+
+#ser.write(b'AT+SAPBR=0,1')
