5096075e4ddd2305df7dec48a2442d60a66fc44c
Sam Stein <sam@mega.net>
2019-06-26 12:59:47 -0700

    Update button_watchdog.py

--- panel/button_watchdog.py
+++ panel/button_watchdog.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 import time
 
 def button_rise(input_pin):
-  print("rise on pin", input_pin)
+    print("rise on pin", input_pin)
 
- def button_fall(input_pin):
-   print("fall on pin", input_pin)
+def button_fall(input_pin):
+    print("fall on pin", input_pin)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
