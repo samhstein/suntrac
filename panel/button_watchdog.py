201ea6a15ac43134536fb9887550a10e351db83f
Sam Stein <sam@mega.net>
2019-06-26 13:08:26 -0700

    Update button_watchdog.py

--- panel/button_watchdog.py
+++ panel/button_watchdog.py
@@ -1,13 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
+pushed = False
 
 def button_push(input_pin):
-    pushed = not pushed
+    global pushed = not pushed
     print("button pushed on pin", input_pin, pushed)
 
-
-pushed = False
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(27, GPIO.BOTH, callback=button_push)
