c4134f44f7c351c994c3952486c929504a0a9977
Sam Stein <sam@mega.net>
2019-06-26 12:47:15 -0700

    Update button_watchdog.py

--- panel/button_watchdog.py
+++ panel/button_watchdog.py
@@ -3,4 +3,5 @@ import RPi.GPIO as GPIO
 def callback_function_print(input_pint):
   print("Input on pin", input_pin)
 
+GPIO.setmode(GPIO.BCM)
 GPIO.add_event_detect(27, GPIO.BOTH, callback=callback_function_print)
