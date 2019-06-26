b22c2cd5a010e02b663dc8f2a654235711ea9a6c
Sam Stein <sam@mega.net>
2019-06-26 13:02:33 -0700

    Update button_watchdog.py

--- panel/button_watchdog.py
+++ panel/button_watchdog.py
@@ -1,14 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 
-def button_rise(input_pin):
-    print("rise on pin", input_pin)
+pushed = False
 
-def button_fall(input_pin):
-    print("fall on pin", input_pin)
+def button_push(input_pin):
+    pushed = not pushed
+    print("button pushed on pin", input_pin, pushed)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(27, GPIO.RISING, callback=button_rise)
-GPIO.add_event_detect(27, GPIO.FALLING, callback=button_fall)
+GPIO.add_event_detect(27, GPIO.BOTH, callback=button_push)
 time.sleep(100)
