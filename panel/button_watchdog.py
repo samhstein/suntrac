a5ab19e9e6c5e79d4971aa9188e1e76d31825a28
Sam Stein <sam@mega.net>
2019-07-02 09:42:14 -0700

    Update button_watchdog.py

--- panel/button_watchdog.py
+++ panel/button_watchdog.py
@@ -1,24 +1,35 @@
 import RPi.GPIO as GPIO
 import time, leds, os
+import signal
 
+run = True
 pushed = False
-down_time = None
+down_time = last_time = None
 push_count = 0
 # get the lights
 leds = leds.leds()
+signal.signal(signal.SIGINT, handler_stop_signals)
+signal.signal(signal.SIGTERM, handler_stop_signals)
+
+def handler_stop_signals(signum, frame):
+    global run
+    run = False
+
+signal.signal(signal.SIGINT, handler_stop_signals)
+signal.signal(signal.SIGTERM, handler_stop_signals)
 
 def button_push(input_pin):
     global pushed
-    global down_time, up_time
+    global down_time, last_time
     global push_count
 
     pushed = not pushed
     print("button pushed on pin", input_pin, pushed)
 
     if pushed:
+        push_count += 1
+        last_time = down_time
         down_time = time.time()
-    else:
-        up_time = time.time()
 
     # long push
     while not GPIO.input(27):
@@ -30,14 +41,16 @@ def button_push(input_pin):
             break
 
     # double push
-    if down_time - up_time < .5:
-        print('double push')
+    if down_time - last_time < .5:
+        print('double push', push_count)
         leds.lights_on(leds.LED_BLUE_OFF, leds.LED_OFF_BLUE)
+    else:
+        push_count = 0
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(27, GPIO.BOTH, callback=button_push, bouncetime=50)
 
-while True:
+while run:
     time.sleep(100)
