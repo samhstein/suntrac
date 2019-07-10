83f6ca6ea24bbbbc1a76edd4a0750001cfb3e898
Sam Stein <sam@mega.net>
2019-07-09 18:44:16 -0700

    Update gps.py

--- gprs/gps.py
+++ gprs/gps.py
@@ -37,11 +37,7 @@ print('ip: ', s)
 
 send_command('AT+CIPGSMLOC=1,1')
 s = ser.read(size=1024)
-print('location: "', s, '"')
-
-send_command('AT+SAPBR=0,1')
-s = ser.read(size=1024)
-print(s)
+print('location: ', s)
 
 send_command('ATZ')
 s = ser.read(size=1024)
