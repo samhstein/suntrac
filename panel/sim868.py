a87142810799e3963975a0842048feed95d8fc00
Sam Stein <sam@mega.net>
2019-07-11 14:36:57 -0700

    Update sim868.py

--- panel/sim868.py
+++ panel/sim868.py
@@ -12,47 +12,47 @@ class sim868:
             print("can't open serial device")
 
 
-    def send_command(self, command):
+    def self.send_command(self, command):
         with_return = str(command) + '\n'
         self.ser.write(with_return.encode('utf-8'))
 
     def get_status(self):
-        send_command('ATZ')
-        s = ser.read(size=1024)
+        self.send_command('ATZ')
+        s = self.ser.read(size=1024)
         print(s)
 
-        send_command('AT+CGATT=1')
-        s = ser.read(size=1024)
+        self.send_command('AT+CGATT=1')
+        s = self.ser.read(size=1024)
         print(s)
 
-        send_command('AT+SAPBR=3,1,"CONTYPE","GPRS"')
-        s = ser.read(size=1024)
+        self.send_command('AT+SAPBR=3,1,"CONTYPE","GPRS"')
+        s = self.ser.read(size=1024)
         print(s)
 
-        send_command('AT+SAPBR=3,1,"APN","jtm2m"')
-        s = ser.read(size=1024)
+        self.send_command('AT+SAPBR=3,1,"APN","jtm2m"')
+        s = self.ser.read(size=1024)
         print(s)
 
-        send_command('AT+SAPBR=0,1')
-        s = ser.read(size=1024)
+        self.send_command('AT+SAPBR=0,1')
+        s = self.ser.read(size=1024)
         print(s)
 
-        send_command('AT+SAPBR=1,1')
-        s = ser.read(size=1024)
+        self.send_command('AT+SAPBR=1,1')
+        s = self.ser.read(size=1024)
         print(s)
 
-        send_command('AT+SAPBR=2,1')
-        s = ser.read(size=1024)
+        self.send_command('AT+SAPBR=2,1')
+        s = self.ser.read(size=1024)
         print('ip: ', s)
 
-        send_command('AT+CIPGSMLOC=1,1')
-        s = ser.read(size=1024)
+        self.send_command('AT+CIPGSMLOC=1,1')
+        s = self.ser.read(size=1024)
         print('location: ', s)
-        s = ser.read(size=1024)
+        s = self.ser.read(size=1024)
         print('location: ', s)
 
-        send_command('ATZ')
-        s = ser.read(size=1024)
+        self.send_command('ATZ')
+        s = self.ser.read(size=1024)
         print(s)
 
         return({"ip": None, "lat": None, "lng": None})
