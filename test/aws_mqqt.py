9391f50c621332a7f524bcc7acc6b58a2d59887f
Sam Stein <sam@mega.net>
2019-07-17 00:43:06 -0700

    Update aws_mqqt.py

--- test/aws_mqqt.py
+++ test/aws_mqqt.py
@@ -10,7 +10,7 @@ streamHandler.setFormatter(formatter)
 logger.addHandler(streamHandler)
 
 
-IOT_ENDPOINT = 'a2z6jgzt0eip8f-ats.iot.us-west-2.amazonaws.com'
+IOT_ENDPOINT = 'a2z6jgzt0eip8f-ats.iot.us-east-1.amazonaws.com'
 
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
@@ -21,12 +21,13 @@ def customCallback(client, userdata, message):
     print("--------------\n\n")
 
 
-myMQTTClient = AWSIoTMQTTClient('beta-1')
+proc_id = '01000b000e43484734303420'
+myMQTTClient = AWSIoTMQTTClient(proc_id)
 myMQTTClient.configureEndpoint(IOT_ENDPOINT, 8883)
 myMQTTClient.configureCredentials(
-    "/home/pi/suntrac/certs/root/AmazonRootCA1.pem",
-    "/home/pi/suntrac/certs/devices/90c9b2ae5d-private.pem.key",
-    "/home/pi/suntrac/certs/devices/90c9b2ae5d-certificate.pem.crt"
+    "/home/pi/suntrac/certs/RootCA.pem",
+    "/home/pi/suntrac/certs/PrivateKey.key",
+    "/home/pi/suntrac/certs/certificatePem.crt"
     )
 
 # AWSIoTMQTTClient connection configuration
@@ -37,9 +38,9 @@ myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
 myMQTTClient.connect()
-myMQTTClient.publish("iot/helo", '{"default": "why do i need this", "proc_id": "beta-1"}', 1)
-myMQTTClient.subscribe("iot/helo", 1, customCallback)
-myMQTTClient.unsubscribe("iot/helo")
+myMQTTClient.publish("suntrac/data", '{"proc_id": proc_id, "raw_data": "hello from device"}', 1)
+myMQTTClient.subscribe("suntrac/data", 1, customCallback)
+myMQTTClient.unsubscribe("suntrac/data")
 myMQTTClient.disconnect()
 
 time.sleep(30)
