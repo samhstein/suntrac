b3f9173027f9bae1f13b65753ccbf67230a35fcc
Sam Stein <sam@mega.net>
2019-07-18 17:20:44 -0700

    Update suntrac.py

--- panel/suntrac.py
+++ panel/suntrac.py
@@ -3,7 +3,7 @@ import sys, json
 
 # get connection to redis
 redis_pub = redis.Redis(host='localhost', port=6379, db=0)
-pub_sub = redus_pubsub()
+pub_sub = redis_pub.pubsub()
 pub_sub.subscribe('suntrac-reading')
 
 for msg in pub_sub.listen():
