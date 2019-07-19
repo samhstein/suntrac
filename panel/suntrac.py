260924d32082b261558f9a67385386c3292c37ad
Sam Stein <sam@mega.net>
2019-07-19 11:40:22 -0700

    Update suntrac.py

--- panel/suntrac.py
+++ panel/suntrac.py
@@ -13,14 +13,14 @@ data_points = []
 def send_to_cloud(data_points):
     print(data_points)
     compressed_points = pickle.dumps(data_points)
-    print(len(compressed_points))
+    print('json: ', len(json.dumps(data_points)))
+    print('pickled: ', len(compressed_points))
     data_points.clear()
     # aws_iot.send(compressed_points)
 
 # eat the first message
 pub_sub.get_message()
 for msg in pub_sub.listen():
-    print('got message: ', msg)
     data_points.append(json.loads(msg['data']))
     if len(data_points) >= 10:
         send_to_cloud(data_points)
