82273c8966f3a14fe328fdf9b2b40dcf083e1677
Sam Stein <sam@mega.net>
2019-07-19 11:52:21 -0700

    Update suntrac.py

--- panel/suntrac.py
+++ panel/suntrac.py
@@ -1,6 +1,6 @@
 import redis, pickle
 import sys, json, time
-import aws_iot
+import aws_iot, zlib
 
 # get aws_iot
 aws_iot = aws_iot.aws_iot()
@@ -12,16 +12,25 @@ data_points = []
 
 def send_to_cloud(data_points):
     print(data_points)
-    compressed_points = pickle.dumps(data_points)
+
+    j_zip = {
+        ZIPJSON_KEY: base64.b64encode(
+            zlib.compress(
+                data_points.encode('utf-8')
+            )
+        ).decode('ascii')
+    }
+
+    return j
     print('json: ', len(json.dumps(data_points)))
-    print('pickled: ', len(compressed_points))
+    print('j_zip: ', len(j_zip))
     data_points.clear()
     # aws_iot.send(compressed_points)
 
 # eat the first message
 pub_sub.get_message()
 for msg in pub_sub.listen():
-    data_points.append(json.loads(msg['data']))
+    data_points.append(msg['data'])
     if len(data_points) >= 10:
         send_to_cloud(data_points)
 
