f6f806ccfa36f5bb32f6fd6e6129a74c3fa63d5e
Sam Stein <sam@mega.net>
2019-07-21 16:01:59 -0700

    Update sim868.py

--- panel/sim868.py
+++ panel/sim868.py
@@ -25,10 +25,9 @@ class sim868:
         self.send_command('ATZ')
         s = self.ser.read(size=1024)
 
-
         self.send_command('AT+GSN')
         s = self.ser.read(size=1024)
-        print('imei', s)
+        imei = s.decode().split('\r\n')[1]
 
         self.send_command('AT+CGATT=1')
         s = self.ser.read(size=1024)
