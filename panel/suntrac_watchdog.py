835c97fe803281a0f1e0904133364ee4987a3af8
Sam Stein <sam@mega.net>
2019-07-21 15:01:09 -0700

    Update suntrac_watchdog.py

--- panel/suntrac_watchdog.py
+++ panel/suntrac_watchdog.py
@@ -67,6 +67,7 @@ pitch = acc_mag.getPitch()
 roll = acc_mag.getRoll()
 
 # read the config file
+config = None
 with open('suntrac.config', 'r') as json_data_file:
     config = json.load(json_data_file)
 
