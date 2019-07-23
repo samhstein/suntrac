0b6b20b06a612e42819646b0c99323fbf16a025e
Sam Stein <sam@mega.net>
2019-07-23 02:52:39 -0700

    Update suntracd.py

--- panel/suntracd.py
+++ panel/suntracd.py
@@ -181,8 +181,8 @@ class SuntracPanel:
             time.sleep(30)
             megaiosun.set_motor(self.RELAY_EAST, 0)
 
-    tl._add_job(publish_panel_data, 10)
-    tl._add_job(get_panel_data, 1)
+    tl._add_job(publish_panel_data, interval=10)
+    tl._add_job(get_panel_data, interval=1)
 
 if __name__ == "__main__":
     sp = SuntracPanel()
