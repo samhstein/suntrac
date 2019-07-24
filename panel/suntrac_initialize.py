a833d6c86d51cea6e6cd7e8f67f267a8c4cc24a1
sam stein <sam@mega.net>
2019-07-24 15:59:04 -0700

    Update suntrac_initialize.py

--- panel/suntrac_initialize.py
+++ panel/suntrac_initialize.py
@@ -4,6 +4,7 @@ import megaiosun
 CONFIG_FILE = '/home/pi/suntrac/panel/suntrac_config.json'
 
 proc_id = megaiosun.get_proc_id()
+print('proc_id: ', proc_id)
 
 with open(CONFIG_FILE, 'r') as json_data_file:
     config = json.load(json_data_file)
@@ -11,10 +12,17 @@ with open(CONFIG_FILE, 'r') as json_data_file:
 aws_iot = aws_iot.aws_iot(proc_id)
 config['proc_id'] = proc_id
 
-aws_iot.sendData('suntrac/config', config)
 # update and write the config file
 with open(CONFIG_FILE, 'w') as json_data_file:
-
     json.dump(config, json_data_file, indent=4)
 
+print('wrote config.')
+
+aws_iot.sendData('suntrac/config', config)
+print('sent config to aws'.)
+
+print('turning of node-red load at boot.')
 os.system('sudo systemctl disable nodered.service')
+
+print('enabling suntrac watchdog')
+os.system('sudo systemctl enable watchdog.service')
